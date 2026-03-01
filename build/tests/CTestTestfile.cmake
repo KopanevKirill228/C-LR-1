971d579ec02728323b8ad4930b38fba3551a3303
Кирилл <kirisgam@gmail.com>
2026-03-01 10:00:25 +0300

    change_README

--- build/tests/CTestTestfile.cmake
+++ build/tests/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(run_tests "C:/Users/kiris/C-LR-1/build/tests/run_tests.exe")
-set_tests_properties(run_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/kiris/C-LR-1/tests/CMakeLists.txt;20;add_test;C:/Users/kiris/C-LR-1/tests/CMakeLists.txt;0;")
+set_tests_properties(run_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/kiris/C-LR-1/tests/CMakeLists.txt;18;add_test;C:/Users/kiris/C-LR-1/tests/CMakeLists.txt;0;")
